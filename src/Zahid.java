3ff5a82d898bb013b2c2329fc4411365a36cdad8
ZahidKhattak <74119320+ZahidKhattak@users.noreply.github.com>
2020-11-14 23:09:18 -0600

    adde more lines

--- src/Zahid.java
+++ src/Zahid.java
@@ -1,4 +1,7 @@
 public class Zahid {
     // added new file on my name to see head
     // hey i am trying
+    public static void main(String[] args) {
+        System.out.println("ZAHID");
+    }
 }
